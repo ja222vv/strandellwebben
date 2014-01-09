529cea14daba0deb228db794165ccb8918ffc7da
Jesper Strandell <ja222vv@student.lnu.se>
2014-01-09 03:53:54 -0500

    validering av telefonnummer

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 document.querySelector("#phone").addEventListener("change",ToggleViewP,false);
 document.querySelector("#mail").addEventListener("change",ToggleViewM,false);
+document.querySelector("#formbutton").addEventListener("click",CorrInput,false);
+
 
 var read_phone = document.querySelector("#phonebox");
 var read_mail = document.querySelector("#mailbox");
@@ -15,4 +17,17 @@ function ToggleViewP() {
         {
             read_phone.classList.toggle("hide");
         }
+}
+function CorrInput(e) {
+    
+    var error_message;
+    var phone_number = document.querySelector("#phone_number").value;
+    
+    if (isNaN(phone_number) === true) {
+        
+        error_message = "Vänligen fyll i ett giltigt telefonnummer.";
+        document.querySelector("#error_message").style.color = "red";
+        document.querySelector("#error_message").innerHTML = error_message;
+        e.preventDefault();
+    }
 }
