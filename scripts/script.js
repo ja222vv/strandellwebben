1ea5443ea5735c319d9e26b278b5750a9857f43a
Jesper Strandell <ja222vv@student.lnu.se>
2014-01-10 10:03:34 -0500

    validering för mail fixad

--- scripts/script.js
+++ scripts/script.js
@@ -7,27 +7,43 @@ var read_phone = document.querySelector("#phonebox");
 var read_mail = document.querySelector("#mailbox");
         
 function ToggleViewM() {
-        if(document.querySelector("#mail").checked === true || document.querySelector("#mail").checked === false)
-        {
-           read_mail.classList.toggle("hide"); 
-        }
+    if(document.querySelector("#mail").checked === true || document.querySelector("#mail").checked === false)
+    {
+        read_mail.classList.toggle("hide"); 
+    }
 }
 function ToggleViewP() {
-        if(document.querySelector("#phone").checked === true || document.querySelector("#phone").checked === false)
-        {
-            read_phone.classList.toggle("hide");
-        }
+    if(document.querySelector("#phone").checked === true || document.querySelector("#phone").checked === false)
+    {
+         read_phone.classList.toggle("hide");
+    }
 }
+//Funtion för validering av vad användaren fyller i på telefonnummer och email.
 function CorrInput(e) {
     
     var error_message;
-    var phone_number = document.querySelector("#phone_number").value;
-    
-    if (isNaN(phone_number) === true) {
+    if(document.querySelector("#phone").checked === true)
+    {
+        var phone_number = document.querySelector("#phone_number").value;
+        if (isNaN(phone_number) === true || phone_number === "") {
         
-        error_message = "Vänligen fyll i ett giltigt telefonnummer.";
-        document.querySelector("#error_message").style.color = "red";
-        document.querySelector("#error_message").innerHTML = error_message;
-        e.preventDefault();
+            error_message = "Vänligen fyll i ett giltigt telefonnummer.";
+            document.querySelector("#error_message").style.color = "red";
+            document.querySelector("#error_message").innerHTML = error_message;
+            e.preventDefault();
+        }
     }
+    if(document.querySelector("#mail").checked === true) {
+        
+        var mail_address = document.querySelector("#email").value;
+        var valid_mail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (valid_mail.test(mail_address) === false) 
+        {
+            error_message = "Vänligen fyll i en korrekt mailadress.";
+            document.querySelector("#error_message").style.color = "red";
+            document.querySelector("#error_message").innerHTML = error_message;
+            e.preventDefault();
+        }
+    }
+    
 }
