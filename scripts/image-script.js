e2584d749e1e488a514f34b29e5e721cc8cc605f
Jesper Strandell <ja222vv@student.lnu.se>
2014-01-15 05:23:44 -0500

    bläddra i bildspel

--- scripts/image-script.js
+++ scripts/image-script.js
@@ -1,25 +1,49 @@
+document.querySelector("#arrowleft").addEventListener("click",LastImage,false);
+document.querySelector("#pause_img").addEventListener("click",StopImages,false);
+document.querySelector("#arrowright").addEventListener("click",NextImage,false);
+
 var ImgArray = ["../Images/Car_XC60.jpg","../Images/Couple.jpg", "../Images/sommarbild.png"];
 
-function MyDelay() {
     var Counter = 0;
+    var MyInterval = setInterval(ChangeImage, 5000);
     
-    setInterval(function() {
-        ChangeImage();
-  
+    //Pausar bildspelet
+    function StopImages() {
+        clearInterval(MyInterval);
+    }
+    //Bläddrar framåt
+    function NextImage() {
+        clearInterval(MyInterval);
+        document.getElementById('slider').src = ImgArray[Counter];
+        if (Counter === ImgArray.length-1) {
+            Counter = 0;
+        }
+        else {
+            Counter++;
+        }
+    }
+    //Bläddrar bakåt
+    function LastImage() {
+        clearInterval(MyInterval);
+        document.getElementById('slider').src = ImgArray[Counter];
+        if (Counter === 0 ) {
+                Counter = ImgArray.length-1;
+            }
+            else {
+                Counter--;
+            }
+    }
+    function ChangeImage() {
+    
+    document.getElementById('slider').src = ImgArray[Counter];
     //Så länge räknaren är mindre än bildarrayens längd, fortsätt...
     if (Counter < ImgArray.length-1) {
         Counter++;
-        console.log(Counter);
     }
     //Annars börja om från början
     else {
         Counter = 0;
     }
-
-}, 5000);
-    function ChangeImage() {
-    document.getElementById('slider').src = ImgArray[Counter]
     }
-}
-//Laddar Delayfunktionen när fönstret laddas.
-window.onload = MyDelay();
+    
+
